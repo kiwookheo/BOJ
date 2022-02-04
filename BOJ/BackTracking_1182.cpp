19fe5757bd54dcc7848171abbb0fdc39db3c6c3f
kiwookheo <goya121@naver.com>
2022-02-04 13:45:11 +0900

    simul

--- BOJ/BackTracking_1182.cpp
+++ BOJ/BackTracking_1182.cpp
@@ -1,26 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-void func(int cur,int sum);
-
-int n, s, cnt=0;
-int arr[20];
-
-int main() {
-	cin >> n >> s;
-	for (int i = 0; i < n; i++) 
-		cin >> arr[i];
-	func(0, 0);
-	if (s == 0) cnt--;
-	cout << cnt;
-}
-
-void func(int cur, int sum) {
-	if (cur == n) {
-		if (sum == s) 
-			cnt++;
-		return;
-	}
-	func(cur + 1, sum);
-	func(cur + 1, sum + arr[cur]);
-}
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//void func(int cur,int sum);
+//
+//int n, s, cnt=0;
+//int arr[20];
+//
+//int main() {
+//	cin >> n >> s;
+//	for (int i = 0; i < n; i++) 
+//		cin >> arr[i];
+//	func(0, 0);
+//	if (s == 0) cnt--;
+//	cout << cnt;
+//}
+//
+//void func(int cur, int sum) {
+//	if (cur == n) {
+//		if (sum == s) 
+//			cnt++;
+//		return;
+//	}
+//	func(cur + 1, sum);
+//	func(cur + 1, sum + arr[cur]);
+//}
