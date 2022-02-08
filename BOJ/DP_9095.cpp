311fe2f743cf21bda21ff514aee56e275af46356
kiwookheo <goya121@naver.com>
2022-02-09 03:11:02 +0900

    DP

--- BOJ/DP_9095.cpp
+++ BOJ/DP_9095.cpp
@@ -1,19 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int n,t; 
-	cin >> t;
-	int d[12];
-	d[1] = 1;
-	d[2] = 2;
-	d[3] = 4;
-	for (int i = 4; i <= 11; i++) {
-		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
-	}
-	while (t--) {
-		cin >> n;
-		cout << d[n] << '\n';
-	}
-	return 0;
-}
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//	int n,t; 
+//	cin >> t;
+//	int d[12];
+//	d[1] = 1;
+//	d[2] = 2;
+//	d[3] = 4;
+//	for (int i = 4; i <= 11; i++) {
+//		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+//	}
+//	while (t--) {
+//		cin >> n;
+//		cout << d[n] << '\n';
+//	}
+//	return 0;
+//}
--- BOJ/Simul_15686.cpp
+++ BOJ/Simul_15686.cpp
@@ -1,38 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std; 
-#define X first
-#define Y second
-
-int n, m;
-int board[52][52];
-vector<pair<int, int>> home;
-vector<pair<int, int>> chic;
-
-int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 1)
-				home.push_back({ i,j });
-			if (board[i][j] == 2)
-				chic.push_back({ i,j });
-		}
-	}
-	vector<int> brute(chic.size(), 1);
-	fill(brute.begin(), brute.begin() + chic.size() - m, 0);
-	int mn = 0x7f7f7f7f;
-	do {
-		int dist = 0;
-		for (auto h : home) {
-			int tmp = 0x7f7f7f7f;
-			for (int i = 0; i < chic.size(); i++) {
-				if (brute[i] == 0) continue;
-				tmp = min(tmp,abs( chic[i].X - h.X ) + abs( chic[i].Y - h.Y ));
-			}
-			dist += tmp;	
-		}
-		mn = min(mn, dist);
-	} while (next_permutation(brute.begin(), brute.end()));
-	cout << mn;
-}
+//#include <bits/stdc++.h>
+//using namespace std; 
+//#define X first
+//#define Y second
+//
+//int n, m;
+//int board[52][52];
+//vector<pair<int, int>> home;
+//vector<pair<int, int>> chic;
+//
+//int main() {
+//	cin >> n >> m;
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			cin >> board[i][j];
+//			if (board[i][j] == 1)
+//				home.push_back({ i,j });
+//			if (board[i][j] == 2)
+//				chic.push_back({ i,j });
+//		}
+//	}
+//	vector<int> brute(chic.size(), 1);
+//	fill(brute.begin(), brute.begin() + chic.size() - m, 0);
+//	int mn = 0x7f7f7f7f;
+//	do {
+//		int dist = 0;
+//		for (auto h : home) {
+//			int tmp = 0x7f7f7f7f;
+//			for (int i = 0; i < chic.size(); i++) {
+//				if (brute[i] == 0) continue;
+//				tmp = min(tmp,abs( chic[i].X - h.X ) + abs( chic[i].Y - h.Y ));
+//			}
+//			dist += tmp;	
+//		}
+//		mn = min(mn, dist);
+//	} while (next_permutation(brute.begin(), brute.end()));
+//	cout << mn;
+//}
